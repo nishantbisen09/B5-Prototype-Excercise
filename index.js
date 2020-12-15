08a23b4a4e85b58106aa6656eed694a93570c180
nishantbisen09 <nishantbisen09@gmail.com>
2020-12-15 22:28:46 +0530

    Implementation | Tasks

--- index.js
+++ index.js
@@ -9,22 +9,21 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-    this.name = name;
-    this.isFlying = false;
-  }
-  Airplane.prototype.takeOff = function () {
-    this.isFlying = true;
-  };
-  Airplane.prototype.land = function () {
-    this.isFlying = false;
-  };
-  
-  
-  /*
+  this.name = name;
+  this.isFlying = false;
+}
+Airplane.prototype.takeOff = function () {
+  this.isFlying = true;
+};
+Airplane.prototype.land = function () {
+  this.isFlying = false;
+};
+
+/*
   // 👇 COMPLETE YOUR WORK BELOW 👇
   */
-  
-  /*
+
+/*
     TASK 1
       - Write a Person Constructor that initializes `name` and `age` from arguments.
       - All instances of Person should initialize with an empty `stomach` array.
@@ -36,18 +35,26 @@ function Airplane(name) {
       - Give instances of Person a method `.toString()`:
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
-  
- function Person() {
-    
-  }
- 
- 
 
-  
-  
-  
-  
-  /*
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+
+Person.prototype.eat = function (food) {
+  if (this.stomach.length !== 10) this.stomach.push(food);
+};
+
+Person.prototype.poop = function () {
+  if (this.stomach.length !== 0) this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+};
+
+/*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
       - All instances built with Car:
@@ -60,32 +67,62 @@ function Airplane(name) {
       - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
-  
- function Car() {
-    
+
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+};
+
+Car.prototype.drive = function (distance) {
+  const maximumPossibleDistance = this.tank * this.milesPerGallon;
+  const fuelRunOutDistance =
+    distance > maximumPossibleDistance ? distance - maximumPossibleDistance : 0;
+  if (fuelRunOutDistance) {
+    this.odometer += maximumPossibleDistance;
+    this.tank -= maximumPossibleDistance / this.milesPerGallon;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  } else {
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
   }
-  
-  
-  /*
+};
+
+const maruti = new Car("maruti", 10);
+maruti.fill(20);
+console.log(maruti.drive(300));
+maruti.fill(10);
+console.log(maruti.drive(300));
+
+/*
     TASK 3
       - Write a Baby constructor subclassing Person.
       - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
- 
-  
-  /* 
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+  this.play = function () {
+    return `Playing with ${this.favoriteToy}`;
+  };
+}
+
+/* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
-  */
-  
-  
-  ///////// END OF CHALLENGE /////////
+    1. Default Binding (In the global execution context , this refers to the global object 'window').
+    2. Implicit Binding (Inside a function, the value of this depends on the calle).
+    3. Explicit Binding (In this case, you can force a function call to use a particular object
+       for this using functions such as call, apply and bind).
+    4. New Binding (when we use new keyword, an empty object is created and referenced by this variable, inheriting the prototype of the function)
+*/
+
+///////// END OF CHALLENGE /////////
